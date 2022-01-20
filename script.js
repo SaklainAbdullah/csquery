71d1ab1b5f215ec7dad03472eee9aefcdacaf14d
Saklain Abdullah <saklain.cuet.cse17@gmail.com>
2022-01-20 17:28:29 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const ml = document.querySelector(".mli")
+const mlp = document.querySelector(".mlp")
+ml.addEventListener('click',()=>{
+    mlp.innerHTML = "Machine learning (ML) is the process of using mathematical models of data to help a computer learn without direct instruction. It’s considered a subset of artificial intelligence (AI). Machine learning uses algorithms to identify patterns within data, and those patterns are then used to create a data model that can make predictions. With increased data and experience, the results of machine learning are more accurate—much like how humans improve with more practice.Since deep learning and machine learning tend to be used interchangeably, it’s worth noting the nuances between the two. Machine learning, deep learning, and neural networks are all sub-fields of artificial intelligence. However, deep learning is actually a sub-field of machine learning, and neural networks is a sub-field of deep learning.UC Berkeley (link resides outside IBM) breaks out the learning system of a machine learning algorithm into three main parts.1. A Decision Process: In general, machine learning algorithms are used to make a prediction or classification. Based on some input data, which can be labelled or unlabeled, your algorithm will produce an estimate about a pattern in the data.2. An Error Function: An error function serves to evaluate the prediction of the model. If there are known examples, an error function can make a comparison to assess the accuracy of the model.3. An Model Optimization Process: If the model can fit better to the data points in the training set, then weights are adjusted to reduce the discrepancy between the known example and the model estimate. The algorithm will repeat this evaluate and optimize process, updating weights autonomously until a threshold of accuracy has been met. "
+
+})
+
+
+
+const ai = document.querySelector(".aii")
+const aip = document.querySelector(".aip")
+ai.addEventListener('click',()=>{
+    aip.innerHTML = "Artificial Intelligence (AI) is rapidly transforming our world. Remarkable surges in AI capabilities have led to a wide range of innovations including autonomous vehicles and connected Internet of Things devices in our homes. AI is even contributing to the development of a brain-controlled robotic arm that can help a paralyzed person feel again through complex direct human-brain interfaces. These new AI-enabled systems are revolutionizing and benefitting nearly all aspects of our society and economy – everything from commerce and healthcare to transportation and cybersecurity. But the development and use of the new technologies it brings are not without technical challenges and risks. "
+
+})
+
+
+const csi = document.querySelector(".csi")
+const csp = document.querySelector(".csp")
+csi.addEventListener('click',()=>{
+    csp.innerHTML = "Computer science is the study of computers, including computational theory, hardware and software design, algorithms and the way humans interact with technology.Computer scientists are problem solvers. When a computer scientist receives a problem, they gather information and communicate with computers using programming languages and logic (e.g. scripting). They create a set of rules or instructions for the computer to execute and solve the problem. Read on to learn more about what computer science is, the importance and benefits of computer science, professional tools and resources, and how to pursue a career in computer science. At its core, a study of computer science includes learning about the “mathematics and the algorithms that are needed to build and solve problems using computers,” says Ian Gorton, director of the computer science master’s programs at Northeastern University—Seattle. It involves an understanding of both the theories and abstract concepts that computing is based in, as well as the use of practical programming skills to build code and develop complex systems.Students who choose to study computer science at the graduate level will learn about the various tools and practices that go into this technology development process, alongside how to deploy their own systems and consistently update and improve those systems over time.Within this broader framework of understanding, students are also often able to choose a specialty in the field. As part of Northeastern’s Master of Science in Computer Science program, for example, students can pick from specialties such as Artificial Intelligence, Information Security, Networks, Computer-Human Interface, Game Design, and more. "
+
+})
+
+
+const dsi = document.querySelector(".dsi")
+const dsp = document.querySelector(".dsp")
+dsi.addEventListener('click',()=>{
+    dsp.innerHTML = "As the name indicates, Data Structure is used for organizing the data in memory. There are various ways of organizing the data in the memory for eg. array, list, stack, queue and many more.Data structure isn’t a programming language like C, C++, java, etc. It is a set of algorithms that can be used in any programming language to organize the data in the memory. "
+
+})
+
+
+const algoi = document.querySelector(".algoi")
+const algop = document.querySelector(".algop")
+algoi.addEventListener('click',()=>{
+    algop.innerHTML = "The current term of choice for a problem-solving procedure, algorithm, is commonly used nowadays for the set of rules a machine (and especially a computer) follows to achieve a particular goal. It does not always apply to computer-mediated activity, however. The term may as accurately be used of the steps followed in making a pizza or solving a Rubik’s Cube as for computer-powered data analysis.Algorithm is often paired with words specifying the activity for which a set of rules have been designed. A search algorithm, for example, is a procedure that determines what kind of information is retrieved from a large mass of data. An encryption algorithm is a set of rules by which information or messages are encoded so that unauthorized persons cannot read them.Though first attested in the early 20th century (and, until recently, used strictly as a term of mathematics and computing), algorithm has a surprisingly deep history.It was formed from algorism “the system of Arabic numerals,” a word that goes back to Middle English and ultimately stems from the name of a 9th-century Persian mathematician, abu-Jaʽfar Mohammed ibn-Mūsa al-Khuwārizmi, who did important work in the fields of algebra and numeric systems. "
+
+})
+
+
+const csui = document.querySelector(".csui")
+csui.addEventListener('click',()=>{
+  csp.innerHTML = ""
+})
+
+const aiui = document.querySelector(".aiui")
+aiui.addEventListener('click',()=>{
+  aip.innerHTML = ""
+})
+
+
+const mlui = document.querySelector(".mlui")
+mlui.addEventListener('click',()=>{
+  mlp.innerHTML = ""
+})
+
+
+const dsui = document.querySelector(".dsui")
+dsui.addEventListener('click',()=>{
+  dsp.innerHTML = ""
+})
+
+
+
+const algoui = document.querySelector(".algoui")
+algoui.addEventListener('click',()=>{
+  algop.innerHTML = ""
+})
